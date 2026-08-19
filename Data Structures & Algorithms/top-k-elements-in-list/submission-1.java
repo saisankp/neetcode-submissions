658a25930759f09758d33ad0d8e3b573f59c2e3e
Prathamesh Sai <saisankp@tcd.ie>
2026-08-19 01:21:31 +0100

    Update: top-k-elements-in-list - submission-1

--- Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
@@ -1,32 +1,39 @@
 class Solution {
-
-    
     public int[] topKFrequent(int[] nums, int k) {
-        //[1,2,2,3,3,3], k = 2
-        //O(N)
-        // 1 -> 1
-        // 2 -> 2
-        // 3 -> 3
-        // number -> frequency
+        // return array = length of k
 
-        // key -> value
-
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
-
+        // 1 = #1
+        // 2 = #2
+        // 3 = #3
+        // .pop() for k times off a Priority queue where we set priority when initializing
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> {
+            // negative → a comes before b
+            // 0        → same priority
+            // positive → b comes before a
+            // If:
+            // a = 5
+            // b = 2
+            // then:
+            // 2 - 5 = -3
+            // Negative → a comes first.
+    return b.getValue() - a.getValue();
+});
         HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int num: nums) {
-            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        for(int i =0; i<nums.length; i++) {
+            int curr = hm.getOrDefault(nums[i], 0);
+            hm.put(nums[i], curr + 1);
         }
 
         for(Map.Entry<Integer, Integer> entry: hm.entrySet()) {
+            // key, value = number, frequency
             pq.add(entry);
         }
 
         int[] result = new int[k];
         for(int i=0; i<result.length; i++) {
-            result[i] = pq.remove().getKey();
+            result[i] = pq.poll().getKey();
         }
-
         return result;
+
     }
 }
